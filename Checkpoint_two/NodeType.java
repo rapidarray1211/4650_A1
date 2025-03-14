eb30aa871bb5ad77fd088c2e7bb776e13ead5e6b
Jubair <mali03@uoguelph.ca>
2025-03-14 16:47:12 -0400

    added constructor to node type

--- Checkpoint_two/NodeType.java
+++ Checkpoint_two/NodeType.java
@@ -4,4 +4,11 @@ public class NodeType {
     public String name;
     public Dec def;
     public int level;
+
+    // Constructor
+    public NodeType(String name, Dec def, int level) {
+        this.name = name;
+        this.def = def;
+        this.level = level;
+    }
 }
