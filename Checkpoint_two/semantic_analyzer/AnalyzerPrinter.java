571f2edd349ff8ac92935a2fb4ae705eb53bace1
Jubair <mali03@uoguelph.ca>
2025-03-15 17:10:04 -0400

    added close

--- Checkpoint_two/semantic_analyzer/AnalyzerPrinter.java
+++ Checkpoint_two/semantic_analyzer/AnalyzerPrinter.java
@@ -74,4 +74,12 @@ public class AnalyzerPrinter {
 
     }
 
+    public void close(){
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
