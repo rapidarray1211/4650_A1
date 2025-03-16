7b9e7ef6b130b2d762cfa4bbf95587da259e6b4e
Jubair <mali03@uoguelph.ca>
2025-03-16 15:03:06 -0400

    added printer and clean Symbol to makefile

---
+++ Checkpoint_two/Symbol/AnalyzerPrinter.java
@@ -0,0 +1,97 @@
+package Symbol;
+
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class AnalyzerPrinter {
+
+    private BufferedWriter writer;
+    private boolean toFile; //write to file or stdout
+    private static final int  SPACES = 4;
+
+    public AnalyzerPrinter(String filename, boolean toFile){
+
+        this.toFile = toFile;
+
+        if (toFile){
+            try {
+                this.writer = new BufferedWriter(new FileWriter(filename));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void indent(int level){
+
+        if (toFile){
+            
+            try {
+                for( int i = 0; i < level * SPACES; i++ ) writer.write( " " );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            for( int i = 0; i < level * SPACES; i++ ) System.out.print( " " );
+        }
+    }
+
+    //print to stderr
+    public void printErr(String error){
+        System.err.println(error);
+    }
+
+    //print message without level
+    public void printMsg(String msg){
+        if (toFile){
+            try {
+                writer.write(msg);
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println(msg);
+        }
+    }
+
+    //print a single message at a level
+    public void printLevel(String msg, int level){
+
+        indent(level);
+        if (toFile){
+            try {
+                writer.write(msg);
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println(msg);
+        }
+
+    }
+
+    //get arrayList of strings and print
+    public void printLevelList(List<String> strings, int level){
+
+        for (String msg : strings){
+            printLevel(msg, level);
+        }
+    }
+
+    public void close(){
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
